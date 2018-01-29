824eb15695a651e62ee015b9ee59ccafc2a02eec
nilutz <nicolutz@macca.local>
2018-01-29 12:55:36 +0100

    ...
    The last few commits are for showing mathjax in gitpages.

--- src/pages/presentation.js
+++ src/pages/presentation.js
@@ -15,11 +15,37 @@ class Presentation extends React.Component {
 
   handleScriptLoad() {
     this.setState({ scriptLoaded: true });
+          MathJax.Hub.Config({
+        TeX: {
+          equationNumbers: {
+            autoNumber: "AMS"
+          }
+        },
+        tex2jax: {
+            inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+            displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+            processEscapes: true,
+            processEnvironments: true
+        },
+        // Center justify equations in code and markdown cells. Elsewhere
+        // we use CSS to left justify single line equations in code cells.
+        displayAlign: 'center',
+        "HTML-CSS": {
+            styles: {'.MathJax_Display': {"margin": 0}},
+            linebreaks: { automatic: true }
+        }
+    });
+      MathJax.Hub.Queue(['Typeset', MathJax.Hub])
   }
   render() {
     return (
       <div>
-
+        <Script
+      url="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_HTML"
+      onCreate={this.handleScriptCreate.bind(this)}
+      onError={this.handleScriptError.bind(this)}
+      onLoad={this.handleScriptLoad.bind(this)}
+    />
         <div dangerouslySetInnerHTML={template} />
       </div>
     );
