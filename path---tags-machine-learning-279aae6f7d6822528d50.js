ab5739c2c9f1bb7f1ca557efcf0b042fa8b96f1b
nilutz <nicolutz@macca.local>
2018-02-19 20:54:04 +0100

    Added two more content folders

--- path---tags-machine-learning-279aae6f7d6822528d50.js
+++ path---tags-machine-learning-279aae6f7d6822528d50.js
@@ -1,2 +1,54 @@
-webpackJsonp([0xe140ce5a49d5],{392:function(e,t){e.exports={data:{allMarkdownRemark:{totalCount:3,edges:[{node:{excerpt:'Deep Learning on Cell Data Set This project is fork from the original "NOAA Fisheries Steller Sea Lion Population Count" Kaggle Challenge…',frontmatter:{title:"Cellcounter",path:"/cellcounter",tags:["programming","deep learning","machine learning"],cover:"img/cellcounter0.png",date:"28/09/2017"}}},{node:{excerpt:"Deep Learning on Kaggle Sea Lion Data Set Together with a friend I participated in the  NOAA Fisheries Steller Sea Lion Population Count…",frontmatter:{title:"Kaggle Challenge: SeaLion Counter",path:"/sealioncounter",tags:["programming","deep learning","machine learning"],cover:"img/sealion0.png",date:"04/08/2017"}}},{node:{excerpt:"Where are the best Pommes ? New to town and just want to have something to eat? The pommesindex to rescue, rate a pommes on your current…",frontmatter:{title:"Pommes Index",path:"/pommesindex",tags:["front-end","back-end","machine learning"],cover:"img/pommes.png",date:"04/04/2017"}}}]}},pathContext:{tag:"machine learning"}}}});
+webpackJsonp([0xe140ce5a49d5], {
+  392: function(e, t) {
+    e.exports = {
+      data: {
+        allMarkdownRemark: {
+          totalCount: 3,
+          edges: [
+            {
+              node: {
+                excerpt:
+                  'Deep Learning on Cell Data Set This project is fork from the original "NOAA Fisheries Steller Sea Lion Population Count" Kaggle Challenge…',
+                frontmatter: {
+                  title: "Cellcounter",
+                  path: "/cellcounter",
+                  tags: ["programming", "deep learning", "machine learning"],
+                  cover: "img/cellcounter0.png",
+                  date: "28/09/2017"
+                }
+              }
+            },
+            {
+              node: {
+                excerpt:
+                  "Deep Learning on Kaggle Sea Lion Data Set Together with a friend I participated in the  NOAA Fisheries Steller Sea Lion Population Count…",
+                frontmatter: {
+                  title: "Kaggle Challenge: SeaLion Counter",
+                  path: "/sealioncounter",
+                  tags: ["programming", "deep learning", "machine learning"],
+                  cover: "img/sealion0.png",
+                  date: "04/08/2017"
+                }
+              }
+            },
+            {
+              node: {
+                excerpt:
+                  "Where are the best Pommes ? New to town and just want to have something to eat? The pommesindex to rescue, rate a pommes on your current…",
+                frontmatter: {
+                  title: "Pommes Index",
+                  path: "/pommesindex",
+                  tags: ["front-end", "back-end", "machine learning"],
+                  cover: "img/pommes.png",
+                  date: "04/04/2017"
+                }
+              }
+            }
+          ]
+        }
+      },
+      pathContext: { tag: "machine learning" }
+    };
+  }
+});
 //# sourceMappingURL=path---tags-machine-learning-279aae6f7d6822528d50.js.map
